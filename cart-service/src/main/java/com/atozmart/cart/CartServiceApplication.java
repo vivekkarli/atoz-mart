3c07cdc2f538326f84fc58d6d8b4bee6bc3f4be1
vivekkarli <vivekkarli7@gmail.com>
2025-02-23 17:48:37 +0530

    setup configserver, eurekaserver, cart-service

--- cart-service/src/main/java/com/atozmart/cart/CartServiceApplication.java
+++ cart-service/src/main/java/com/atozmart/cart/CartServiceApplication.java
@@ -1,14 +1,13 @@
-package com.atozmart.catalog;
+package com.atozmart.cart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CatalogServiceApplication {
-	
+public class CartServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CatalogServiceApplication.class, args);
+		SpringApplication.run(CartServiceApplication.class, args);
 	}
 
 }
