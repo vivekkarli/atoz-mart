459484d45d7a1d688b1015694f9f6bd78f90e993
vivekkarli <vivekkarli7@gmail.com>
2025-03-16 20:48:49 +0530

    remove correlation id

--- catalog-service/src/main/java/com/atozmart/catalog/controller/CatalogController.java
+++ catalog-service/src/main/java/com/atozmart/catalog/controller/CatalogController.java
@@ -2,22 +2,16 @@ package com.atozmart.catalog.controller;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.atozmart.catalog.dto.ItemDto;
-import com.atozmart.catalog.entity.Item;
 import com.atozmart.catalog.exception.CatalogException;
-import com.atozmart.catalog.repository.ItemRepository;
 import com.atozmart.catalog.service.CatalogService;
 
 import lombok.AllArgsConstructor;
@@ -27,14 +21,11 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class CatalogController {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(CatalogController.class);
-
 	private CatalogService catalogService;
 
 	@GetMapping(value = "/items/all", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<List<ItemDto>> viewAllItems(@RequestHeader("atozmart-correlation-id") String correlationId)
+	public ResponseEntity<List<ItemDto>> viewAllItems()
 			throws CatalogException {
-		LOGGER.debug("atozmart-correlation-id: {}", correlationId);
 		List<ItemDto> itemDtos = catalogService.getItems();
 		return ResponseEntity.ok().body(itemDtos);
 	}
@@ -44,10 +35,8 @@ public class CatalogController {
 	public ResponseEntity<List<ItemDto>> viewItems(@RequestParam(name = "page", defaultValue = "0") int pageNo,
 			@RequestParam(defaultValue = "10") int size,
 			@RequestParam(name = "sort-by", defaultValue = "name") String sortBy,
-			@RequestParam(name = "direction", defaultValue = "asc") String sortDirection,
-			@RequestHeader("atozmart-correlation-id") String correlationId) throws CatalogException {
+			@RequestParam(name = "direction", defaultValue = "asc") String sortDirection) throws CatalogException {
 
-		LOGGER.debug("atozmart-correlation-id: {}", correlationId);
 		List<ItemDto> itemDtos = catalogService.getItems(pageNo, size, sortBy, sortDirection);
 		return ResponseEntity.ok().body(itemDtos);
 	}
