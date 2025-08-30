55a62d92a2ce852c4f985dce152ea0b01f0cc718
vivekkarli <vivekkarli7@gmail.com>
2025-08-30 18:23:08 +0530

    change config server port

--- catalog-service/src/main/java/com/atozmart/catalog/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/atozmart/catalog/CatalogServiceApplication.java
@@ -3,11 +3,8 @@ package com.atozmart.catalog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = { "com.atozmart.catalog"
-		, "com.atozmart.commons" 
-		})
+@SpringBootApplication(scanBasePackages = { "com.atozmart.catalog", "com.atozmart.commons" })
 public class CatalogServiceApplication {
-	
 
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogServiceApplication.class, args);
